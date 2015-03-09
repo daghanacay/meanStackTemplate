b021e253c9f12c2e57bcd6226aabf1a3293b3d51
daghan_acay <lacay@unimelb.edu.au>
2015-03-09 13:38:43 +1100

    cleaning up the additional model file

--- app/models/gadget.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose')
-
-module.exports =  mongoose.model('Gadget',
-  {
-    'name' : {'type':'String','default':'default gadget'},
-    'belongsToNerd': {'type':'String'}
-  });
