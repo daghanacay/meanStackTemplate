d4b78455c05fa60b096b40412fce8745a5765d0b
daghan_acay <lacay@unimelb.edu.au>
2015-03-10 16:47:48 +1100

    wiring up controler and service as well as the view accordingly

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,4 +1,9 @@
 // public/js/controllers/NerdCtrl.js
-angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+angular.module('NerdCtrl', []).controller('NerdController', ['$scope', 'NerdsSrv', function($scope,NerdsSrv) {
     $scope.tagline = 'Nothing beats a pocket protector!';
-});
+    //$scope.nerds = [{"name":"static name 1"},{"name":"static name 2"}];
+
+    NerdsSrv.get().success(function(data) {
+                $scope.nerds = data;
+            });
+}]);
