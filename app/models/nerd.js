12d1ed5c384acd6f93ec93cae811a39f98280a85
daghan_acay <lacay@unimelb.edu.au>
2015-03-12 11:29:54 +1100

    adding the extra addres to the model and the router. also updated the view to display extra information in a more appealing way.

--- app/models/nerd.js
+++ app/models/nerd.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Nerd', {
-    name : {type : String, default: ''}
+    name : {type : String, default: ''},
+    address : {
+	  city : {type:String, required: false},
+          street : {type:String, required: false}
+	}
 });
-
